e9c43ba839b93252f85984cb523107cecb2e7108
Valen <valenpschool@gmail.com>
2023-04-14 10:20:05 -0300

    feat: add lógica inicial

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const textoInput = document.getElementById("texto");
+const resultado = document.querySelector(".resultado");
+const btnDescripto = document.querySelector(".descriptografa");
+const btnCripto = document.querySelector(".criptografa");
+// a -> ai
+// e -> enter
+// i -> imes
+// o -> ober
+// u -> ufat
+
+function criptografa() {
+  let texto = textoInput.value;
+  texto.   
+  console.log(texto);
+}
+
+function descriptografa() {
+  
+
+}
+
+    btnCripto.onclick = criptografa;
+    btnDescripto.onclick = descriptografa;
+
+
