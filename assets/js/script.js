2e5ea5bf0332e540783b4ced3e20a598fd5538e3
Valen <valenpschool@gmail.com>
2023-04-14 16:37:11 -0300

    feat: add lógica de criptografar/descriptografar (funcionando)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,27 @@ const textoInput = document.getElementById("texto");
 const resultado = document.querySelector(".resultado");
 const btnDescripto = document.querySelector(".descriptografa");
 const btnCripto = document.querySelector(".criptografa");
-// a -> ai
-// e -> enter
-// i -> imes
-// o -> ober
-// u -> ufat
 
 function criptografa() {
   let texto = textoInput.value;
-  texto.   
-  console.log(texto);
+  texto = texto.replace(/e/g, "enter");
+  texto = texto.replace(/o/g, "ober");
+  texto = texto.replace(/i/g, "imes");
+  texto = texto.replace(/a/g, "ai");
+  texto = texto.replace(/u/g, "ufat");
+
+  
+  return resultado.innerHTML = texto;
 }
 
 function descriptografa() {
-  
+  let texto = textoInput.value;
+  texto = texto.replace(/ai/g, "a");
+  texto = texto.replace(/enter/g, "e");
+  texto = texto.replace(/imes/g, "i");
+  texto = texto.replace(/ober/g, "o");
+  texto = texto.replace(/ufat/g, "u");
+  return resultado.innerHTML = texto;
 
 }
 
