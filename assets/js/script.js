02146f4a24c0948325e7396565b169873895d08c
Valen <valenpschool@gmail.com>
2023-04-15 22:34:20 -0300

    refactor: arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,35 @@
 const textoInput = document.getElementById("texto");
 const resultado = document.querySelector(".resultado");
-const btnDescripto = document.querySelector(".descriptografa");
-const btnCripto = document.querySelector(".criptografa");
+const btnDescripto = document.querySelector(".btn-descriptografa");
+const btnCripto = document.querySelector(".btn-criptografa");
 
 function criptografa() {
+  const criptografos = ["ai", "enter", "imes", "ober", "ufat"];
   let texto = textoInput.value;
   texto.toLowerCase();
-  texto = texto.replace(/e/g, "enter");
-  texto = texto.replace(/o/g, "ober");
-  texto = texto.replace(/i/g, "imes");
-  texto = texto.replace(/a/g, "ai");
-  texto = texto.replace(/u/g, "ufat");
+  texto = texto.replace(/e/g, criptografos[1]);
+  texto = texto.replace(/o/g, criptografos[3]);
+  texto = texto.replace(/i/g, criptografos[2]);
+  texto = texto.replace(/a/g, criptografos[0]);
+  texto = texto.replace(/u/g, criptografos[4]);
   
   
   return resultado.innerHTML = texto;
 }
 
 function descriptografa() {
+  const vogais = ["a", "e", "i", "o", "u"];
   let texto = textoInput.value;
   texto.toLowerCase();
-  texto = texto.replace(/ai/g, "a");
-  texto = texto.replace(/enter/g, "e");
-  texto = texto.replace(/imes/g, "i");
-  texto = texto.replace(/ober/g, "o");
-  texto = texto.replace(/ufat/g, "u");
+  texto = texto.replace(/ai/g, vogais[0]);
+  texto = texto.replace(/enter/g, vogais[1]);
+  texto = texto.replace(/imes/g, vogais[2]);
+  texto = texto.replace(/ober/g, vogais[3]);
+  texto = texto.replace(/ufat/g, vogais[4]);
   return resultado.innerHTML = texto;
 
 }
 
-    btnCripto.onclick = criptografa;
-    btnDescripto.onclick = descriptografa;
-
+     btnCripto.onclick = criptografa;
+     btnDescripto.onclick = descriptografa;
 
