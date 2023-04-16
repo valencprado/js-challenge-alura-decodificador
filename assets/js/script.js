a27fcb7bae8166f95dfb02273063ea72e3fb1ccc
Valen <valenpschool@gmail.com>
2023-04-15 23:44:09 -0300

    feat: ocultar imagens, copiar texto

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,29 @@ const textoInput = document.getElementById("texto");
 const resultado = document.querySelector(".resultado");
 const btnDescripto = document.querySelector(".btn-descriptografa");
 const btnCripto = document.querySelector(".btn-criptografa");
+const conteudoResultado = document.querySelector(".content-container-resultado");
+const textoResultado = document.querySelector(".resultado");
+const btnCopiar = document.getElementById("btn-copiar");
+btnCopiar.style.display = "none";
 
+function oculta() {
+  conteudoResultado.style.display = "none";
+  if(btnCopiar.style.display === "none") {
+    btnCopiar.style.display = "block";
+  }
+}
+
+function copia() {
+  textoInput.select();
+  navigator.clipboard.writeText(textoResultado.innerText)
+  .then(() => {
+    alert("Texto copiado com sucesso!");
+  })
+  .catch((err) => {
+    alert("Ops! Algo deu errado ao copiar o texto...");
+    console.log(err);
+  });
+}
 function criptografa() {
   const criptografos = ["ai", "enter", "imes", "ober", "ufat"];
   let texto = textoInput.value;
@@ -13,7 +35,7 @@ function criptografa() {
   texto = texto.replace(/a/g, criptografos[0]);
   texto = texto.replace(/u/g, criptografos[4]);
   
-  
+  oculta();
   return resultado.innerHTML = texto;
 }
 
@@ -26,10 +48,12 @@ function descriptografa() {
   texto = texto.replace(/imes/g, vogais[2]);
   texto = texto.replace(/ober/g, vogais[3]);
   texto = texto.replace(/ufat/g, vogais[4]);
+  oculta();
   return resultado.innerHTML = texto;
-
+  
 }
 
      btnCripto.onclick = criptografa;
      btnDescripto.onclick = descriptografa;
+     btnCopiar.onclick = copia;
 
